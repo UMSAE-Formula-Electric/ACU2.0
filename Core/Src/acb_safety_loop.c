f38a969fbcd6e0f04cd4f6d04dc3745e6e46018f
Ángel Donat Thobi <craset832@gmail.com>
2026-05-30 15:31:52 -0500

    Changed the safety loop so taht it reads twice before updating the state (debouncing)

--- Core/Src/acb_safety_loop.c
+++ Core/Src/acb_safety_loop.c
@@ -2,13 +2,29 @@
 #include "gpio.h"
 
 /**
-  * @brief  Starts the can message recieve and processing task
+  * @brief  Starts the can message receive and processing task
   * @retval SAFTEY_LOOP_OPEN if loop is open, SAFTEY_LOOP_CLOSED if loop is closed)
   */
 loop_status_t check_safety_loop(){
-	uint8_t loop_status = SAFETY_LOOP_OPEN;
-	if(HAL_GPIO_ReadPin(SHCT_TAP_GPIO_Port, SHCT_TAP_Pin)){
-		loop_status = SAFETY_LOOP_CLOSED;
-	}
-	return loop_status;
+
+	// this is to keep track of the last known valid state
+	static loop_status_t last_valid_state = SAFETY_LOOP_OPEN;
+
+	// sampling twice with a 15ms delay
+	GPIO_PinState sample1 = HAL_GPIO_ReadPin(SHCT_TAP_GPIO_Port, SHCT_TAP_Pin);
+	HAL_Delay(15);
+	GPIO_PinState sample2 = HAL_GPIO_ReadPin(SHCT_TAP_GPIO_Port, SHCT_TAP_Pin);
+
+
+	// If both samples match update the state,
+	// if not matching, means a bounce occurred
+	if (sample1 == sample2) {
+	        if (sample1 == GPIO_PIN_SET) {
+	            last_valid_state = SAFETY_LOOP_CLOSED;
+	        } else {
+	            last_valid_state = SAFETY_LOOP_OPEN;
+	        }
+	    }
+
+	return last_valid_state;
 }
